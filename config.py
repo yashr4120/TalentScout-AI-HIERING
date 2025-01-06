b0b9b26309f3fa26c886e54c49dc27a671b6feeb
YASH RANA <121223761+yashr4120@users.noreply.github.com>
2025-01-06 13:34:23 +0530

    config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("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")
+GROQ_API_KEY = os.getenv("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")
 MODEL_NAME = "mixtral-8x7b-32768" 
